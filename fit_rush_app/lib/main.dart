50769d675cf5ec0e410d1100b215cc4785a78d67
MohamedAwad33 <95631364+MohamedAwad33@users.noreply.github.com>
2025-05-12 18:10:39 +0300

    providing cubits

--- fit_rush_app/lib/main.dart
+++ fit_rush_app/lib/main.dart
@@ -1,5 +1,9 @@
-import 'package:fit_rush_app/screens/home_screen.dart';
+import 'package:fit_rush_app/cubits/health_permissions_cubit/health_permissions_cubit.dart';
+import 'package:fit_rush_app/cubits/last_seven_days_health_cubit/last_seven_days_health_cubit.dart';
+import 'package:fit_rush_app/cubits/today_health_cubit/today_health_cubit.dart';
+import 'package:fit_rush_app/views/screens/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,13 +15,20 @@ class FitRushApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'FitRush',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => TodayHealthCubit()..fetchTodayData()),
+        BlocProvider(create: (context) => LastSevenDaysHealthCubit()..fetchWeekData()),
+        BlocProvider(create: (context) => HealthPermissionsCubit()..requestPermissions()),
+      ],
+      child: MaterialApp(
+        title: 'FitRush',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        ),
+        home: HomeScreen(),
       ),
-      home: HomeScreen(),
     );
   }
 }
