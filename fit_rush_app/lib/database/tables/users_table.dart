c94e2e02fbd403c49b7c6f9b755c8cdbfc5d9b72
MohamedAwad33 <95631364+MohamedAwad33@users.noreply.github.com>
2025-05-13 18:27:04 +0300

    add users table and user goal model and it's converter

---
+++ fit_rush_app/lib/database/tables/users_table.dart
@@ -0,0 +1,13 @@
+import 'package:drift/drift.dart';
+import 'package:fit_rush_app/database/converters/user_goal_type_converter.dart';
+
+class UsersTable extends Table {
+  IntColumn get id => integer().autoIncrement()();
+  TextColumn get name => text()();
+  IntColumn get age => integer().nullable()();
+  TextColumn get gender => text().nullable()();
+  RealColumn get height => real().nullable()();
+  RealColumn get weight => real().nullable()();
+  TextColumn get longTermGoal => text().map(const UserGoalTypeConverter())();
+  TextColumn get dailyGoal => text().map(const UserGoalTypeConverter())();
+}
