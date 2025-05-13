ac58d8c850a2403882d409238ff4b05c116e0951
MohamedAwad33 <95631364+MohamedAwad33@users.noreply.github.com>
2025-05-13 19:22:25 +0300

    modifying health permissions cubit and change UI based on it

--- fit_rush_app/lib/cubits/health_permissions_cubit/health_permissions_cubit.dart
+++ fit_rush_app/lib/cubits/health_permissions_cubit/health_permissions_cubit.dart
@@ -1,15 +1,31 @@
 import 'package:fit_rush_app/cubits/health_permissions_cubit/health_permissions_cubit_states.dart';
 import 'package:fit_rush_app/services/health_service.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class HealthPermissionsCubit extends Cubit<HealthPermissionsState> {
   HealthPermissionsCubit() : super(HealthPermissionsInitial());
 
-  Future<void> requestPermissions() async {
+  Future<void> requestPermissionsOnce() async {
     emit(HealthPermissionsLoading());
-    try {
-      await HealthService.requestHealthPermissions();
+
+    final prefs = await SharedPreferences.getInstance();
+    final alreadyRequested =
+        prefs.getBool('hasRequestedHealthPermission') ?? false;
+
+    if (alreadyRequested) {
       emit(HealthPermissionsGranted());
+      return;
+    }
+
+    try {
+      final granted = await HealthService.requestHealthPermissions();
+      if (granted) {
+        await prefs.setBool('hasRequestedHealthPermission', true);
+        emit(HealthPermissionsGranted());
+      } else {
+        emit(HealthPermissionsDenied(errorMessage: "Permissions not granted"));
+      }
     } catch (e) {
       emit(HealthPermissionsDenied(errorMessage: "Permissions not granted"));
     }
