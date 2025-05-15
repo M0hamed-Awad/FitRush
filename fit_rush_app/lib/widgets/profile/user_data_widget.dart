4931ef870969fbdb3a103d3ed2ca808f9e05efc6
MohamedAwad33 <95631364+MohamedAwad33@users.noreply.github.com>
2025-05-15 08:37:03 +0300

    update user profile based on real user data

--- fit_rush_app/lib/widgets/profile/user_data_widget.dart
+++ fit_rush_app/lib/widgets/profile/user_data_widget.dart
@@ -2,13 +2,12 @@ import 'package:fit_rush_app/widgets/profile/goal_text_field.dart';
 import 'package:flutter/material.dart';
 
 class UserDataWidget extends StatelessWidget {
-  const UserDataWidget({super.key});
+  final double height, weight;
+  const UserDataWidget({super.key, required this.height, required this.weight});
 
   @override
   Widget build(BuildContext context) {
-    return
-
-    Padding(
+    return Padding(
       padding: const EdgeInsets.symmetric(vertical: 32.0),
       child: GridView.count(
         crossAxisCount: 2, // 2 columns
@@ -18,8 +17,8 @@ class UserDataWidget extends StatelessWidget {
         shrinkWrap: true,
         physics: NeverScrollableScrollPhysics(),
         children: [
-          GoalTextField(label: "Height", data: 175),
-          GoalTextField(label: "Weight", data: 75),
+          GoalTextField(label: "Height", data: height),
+          GoalTextField(label: "Weight", data: weight),
           GoalTextField(label: "BMI", data: 25),
         ],
       ),
