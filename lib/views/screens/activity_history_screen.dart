5534bdcc952c6f78dd753c3bcdc834360152e12c
MohamedAwad33 <95631364+MohamedAwad33@users.noreply.github.com>
2025-05-20 04:11:48 +0300

    change history screen to activity history screen

--- lib/views/screens/activity_history_screen.dart
+++ lib/views/screens/activity_history_screen.dart
@@ -1,8 +1,8 @@
 import 'package:fit_rush_app/widgets/history/history_screen_body.dart';
 import 'package:flutter/material.dart';
 
-class HistoryScreen extends StatelessWidget {
-  const HistoryScreen({super.key});
+class ActivityHistoryScreen extends StatelessWidget {
+  const ActivityHistoryScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
--- lib/widgets/home/home_screen_body.dart
+++ lib/widgets/home/home_screen_body.dart
@@ -4,7 +4,7 @@ import 'package:fit_rush_app/cubits/user_cubit/user_cubit.dart';
 import 'package:fit_rush_app/cubits/user_cubit/user_cubit_states.dart';
 import 'package:fit_rush_app/helper/navigation_helper.dart';
 import 'package:fit_rush_app/styles/sizes.dart';
-import 'package:fit_rush_app/views/screens/history_screen.dart';
+import 'package:fit_rush_app/views/screens/activity_history_screen.dart';
 import 'package:fit_rush_app/widgets/common/custom_progress_bar.dart';
 import 'package:fit_rush_app/widgets/home/daily_activity_progress_rings.dart';
 import 'package:fit_rush_app/widgets/home/daily_activity_stats.dart';
@@ -81,7 +81,7 @@ class HomeScreenBody extends StatelessWidget {
                       HistoryNavigateCard(
                         onTap: () {
                           NavigationHelper.push(
-                            destination: HistoryScreen(),
+                            destination: ActivityHistoryScreen(),
                             context: context,
                           );
                         },
